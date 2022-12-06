c1d7d8b962f1376d7cc1ccf3e34455ed1b7dba1b
excelpython2022 <117869323+excelpython2022@users.noreply.github.com>
2022-12-06 17:49:26 +0700

    Create TachSo.vb

---
+++ TachSo.vb
@@ -0,0 +1,8 @@
+Function TachSo(CellRef As String)
+Dim StringLength As Integer
+StringLength = Len(CellRef)
+For i = 1 To StringLength
+If IsNumeric(Mid(CellRef, i, 1)) Then Result = Result & Mid(CellRef, i, 1)
+Next i
+TachSo = Format(Result, "#,###")
+End Function
